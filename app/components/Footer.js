5d67a9f3e03a51febf0271719e10c785e33d4c68
Ana Arevadze <anaarevadze@Anas-MacBook-Air.local>
2025-06-19 15:36:33 +0400

    footer with contact info implementation

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,14 +2,17 @@ import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="flex justify-center space-x-6 text-gray-600 mt-16">
-      <a href="https://github.com/yourgithub" target="_blank">
-        <FaGithub size={20} />
+      <a href="https://github.com/Anaare" target="_blank">
+        <FaGithub size={30} />
       </a>
-      <a href="https://linkedin.com/in/yourlinkedin" target="_blank">
-        <FaLinkedin size={20} />
+      <a
+        href="https://www.linkedin.com/in/ana-arevadze-47b116332/"
+        target="_blank"
+      >
+        <FaLinkedin size={30} />
       </a>
       <a href="mailto:aarevadze41@gmail.com">
-        <FaEnvelope size={20} />
+        <FaEnvelope size={30} />
       </a>
     </footer>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./about/page";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="flex flex-col gap-20 px-6 max-w-6xl mx-auto">
         <About />
       </div>
+      <Footer />
     </div>
   );
 }
